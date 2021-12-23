0c59e997745f882cf3a5c97e987a12e945971479
unknown <habibighofrane0000@gmail.com>
2021-12-23 21:29:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import {MovieList } from './Components/MovieList';
+import { movieData } from './Components/Data';
+import AddMovie from './Components/AddMovie';
+import {Navbar, Nav, Container} from 'react-bootstrap'
+import FilterByName from './Components/FilterByName';
+import RatingStarFilter from './RatingStar'
+import MyFooter from './Components/MyFooter';
+
+
+
 
 function App() {
+  const [movies, setMovies] = useState(movieData);
+  const [inputSearch, setInputSearch] = useState("");
+  const [starRate, setStarRate] = useState(0);
+
+    const add = (newMovie) => {
+      setMovies ([...movies, newMovie])
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">MOVIE APP</Navbar.Brand>
+            <Nav className="me-auto">
+              <AddMovie className= "addBtn" add= {add}/>
+              <FilterByName  inputSearch={inputSearch} setInputSearch={setInputSearch}/>
+              <div className="StarsFilter">
+              <RatingStarFilter isMovieRating={false} starRate={starRate} setStarRate={setStarRate} />
+              </div>
+            </Nav>
+        </Container>
+      </Navbar>
+          <div className="MovieList">
+            <MovieList movies={movies} inputSearch={inputSearch} starRate={starRate}/>
+          </div>
+          <MyFooter/>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
